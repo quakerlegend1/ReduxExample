ad6e41d73255804ee8f39d5e185d37c57728c1b4
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-25 19:02:38 +0300

    qq

--- src/ReduxSideBar.jsx
+++ src/ReduxSideBar.jsx
@@ -4,7 +4,7 @@ export function ReduxSideBar({arr}) {
     return (
         <div className={styles.container}>
             <ul>
-            {arr.map((el,index)=>(<li key={index} className={styles.element}><span>Vite + React</span></li>))}
+            {arr.map((el,index)=>(<li key={index} className={styles.element}><span>Redux</span></li>))}
             </ul>
         </div>
     )
--- src/SideBarUseState.jsx
+++ src/SideBarUseState.jsx
@@ -4,7 +4,7 @@ export function SidebarUseState({arr}) {
     return ( 
         <div className={styles.container}>
             <ul>
-            {arr.map((el,index)=>(<li key={index} className={styles.element}><span>Vite + React</span></li>))}
+            {arr.map((el,index)=>(<li key={index} className={styles.element}><span>useState</span></li>))}
             </ul>
         </div>
     )
